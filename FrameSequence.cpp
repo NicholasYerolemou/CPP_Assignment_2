c0bd82c083a24d78ce89c4c27bb15477deffda36
NicholasYerolemou <yerolemou.nicholas@gmail.com>
2022-03-14 22:48:50 +0200

    reads image in and creates the correct frames

--- FrameSequence.cpp
+++ FrameSequence.cpp
@@ -4,9 +4,10 @@
 #include <fstream>
 #include <ios>
 #include <sstream>
+
 // stores a single frame image
-// imageSequence holds the entire "video" each element in the array is itself an array that regresents one frame in the "video"
-// the element is a dynamically allocated unsigned char array
+//  imageSequence holds the entire "video" each element in the array is itself an array that regresents one frame in the "video"
+//  the element is a dynamically allocated unsigned char array
 yrlnic001::FrameSequence::FrameSequence(void) : imageSequence() // initialises the vector
 {
     // set vector to 0
@@ -14,6 +15,13 @@ yrlnic001::FrameSequence::FrameSequence(void) : imageSequence() // initialises t
 
 yrlnic001::FrameSequence::~FrameSequence()
 {
+    delete[] image; /*
+     for (int i = 0; i < sizeof(imageSequence); i++)
+     {
+         delete imageSequence[i];
+     }
+     */
+    // removes the image from memory
     // delete dynamic memory
     // run through imageSequcnce and delete each dynamic sub array. (by deleteing each chat in it?)
 }
@@ -38,10 +46,90 @@ void yrlnic001::FrameSequence::setOperations(char *ops[], int numArgs) // array 
     // finish this
 }
 
-void yrlnic001::FrameSequence::addFrame(unsigned char **frame)
+void yrlnic001::FrameSequence::readInFile(std::string filename)
 {
+    std::ifstream myFile;
+
+    myFile.open(filename, std::ios::binary); // reads the file in as binary
+    std::string line;
+    getline(myFile, line); // p5 always first in the file
+    getline(myFile, line);
+    while (line[0] == '#') // gets rid of comment lines
+    {
+        getline(myFile, line);
+    }
+
+    std::istringstream iss(line);
+    int rows, columns;
+    iss >> columns;
+    iss >> rows;
+
+    std::string temp;
+    myFile >> temp >> std::ws;
+    // pointer to the array that holds the image
+    int size = rows * columns;
+    std::cout << size << std::endl;
+    image = new unsigned char[size]; // make this private in the future
+
+    myFile.read((char *)image, size);
+    if (myFile)
+        std::cout << "all characters read successfully." << std::endl;
+    myFile.close();
+    std::ofstream outfile("new.pgm", std::ofstream::binary);
+    outfile << "P5\n"
+            << columns << " " << rows << "\n"
+            << "255"
+            << "\n";
+    outfile.write((char *)image, size);
+    outfile.close();
+}
+
+void yrlnic001::FrameSequence::createVideo()
+{ // starts the rect in the given pos and loops until it reaches the edge on the other end
+
+    int size = width * height; // size of rectangle
+    unsigned char *frame = new unsigned char[size];
+    int counter = 0;
+
+    for (int y = y1; y < y1 + height; y++)
+    {
+
+        for (int x = x1; x < x1 + width; x++)
+        {
+            frame[counter] = image[x1 * y1];
+        }
+    }
+
+    addFrame(&frame);
+
+    if (x1 != x2 && y1 != y2)
+    {
+        x1 = x1 + 1;
+        y1 = y1 + 1;
+        createVideo();
+    }
+}
+
+void yrlnic001::FrameSequence::addFrame(unsigned char **frame)
+{ // frame is a dynamically allocated 2d image frame
     yrlnic001::FrameSequence::imageSequence.push_back(frame);
 }
+
+void yrlnic001::FrameSequence::test()
+{
+    int i = 10;
+    int row = 2;
+    int col = 15;
+    std::cout << imageSequence[i][row][col];
+}
+
+void yrlnic001::FrameSequence::storeImage(std::ifstream &File) // recieve refernce to array that holds the image in unsigned chars
+{
+    std::string line;
+    getline(File, line); // p5 always first in the file
+    std::cout << line;
+}
+
 int main(int argc, char *argv[]) // argv is a an array
 {                                // check input for errors
     yrlnic001::FrameSequence myObj;
@@ -49,47 +137,9 @@ int main(int argc, char *argv[]) // argv is a an array
     myObj.setCoords(std::stoi(argv[3]), std::stoi(argv[4]), std::stoi(argv[5]), std::stoi(argv[6]));
     myObj.setRectSize(std::stoi(argv[8]), std::stoi(argv[9]));
     myObj.setOperations(argv, argc);
-
+    myObj.readInFile(imageName);
     // read in file
-    std::ifstream myFile;
-    myFile.open(imageName, std::ios::binary); // reads the file in as binary
-    std::string line;
-    getline(myFile, line); // p5 always first in the file
-    getline(myFile, line);
 
-    while (line[0] == '#') // gets rid of comment lines
-    {
-        getline(myFile, line);
-    }
-    int rows, columns;
-    std::stringstream ss(line); // this line holds the number of rows and columns of unsigned chars
-    std::string word;
-    ss >> word;
-    rows = std::stoi(word);
-    ss >> word;
-    columns = std::stoi(word);
-
-    getline(myFile, line);
-    unsigned char frame[rows * columns]; // array holds one frame with rows*columns pixels
-    int counter = 0;
-    unsigned char i;
-    while (!myFile.eof()) // add each unsigned chat (pixel) to array
-    {
-        myFile >> i >> std::ws;
-        frame[counter] = i;
-        counter++;
-    }
-    myObj.addFrame(frame) // move pointer of class variable to this array
-
-    /*
-        while (!myFile.eof())
-        {
-
-            while (line[0] == '#') // loop through comments
-            {
-                getline(myFile, line);
-                // std::cout << line << std::endl;
-            }
-        }
-    */
+    myObj.createVideo();
+    myObj.~FrameSequence();
 }
--- FrameSequence.h
+++ FrameSequence.h
@@ -15,12 +15,19 @@ namespace yrlnic001
         void setCoords(int x, int y, int xx, int yy);
         void setRectSize(int w, int h);
         void setOperations(char *ops[], int numArgs);
+        void readInFile(std::string filename);
         void addFrame(unsigned char **frame);
+        void storeImage(std::ifstream &File);
+        void createVideo();
+        void WriteVideo();
+        void test();
 
     private:
         std::vector<unsigned char **> imageSequence; // each position points to an array that holds an unsigned char at each position
+        // unsigned char **image = NULL; // pointer to the array that holds the entire image
         int x1, y1, x2, y2;
         int width, height;
+        unsigned char *image = NULL;
         struct w
         {
             std::string operation; // operation to be done to data
